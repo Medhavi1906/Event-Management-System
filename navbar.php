1fc9e3f16c9f14ca34cde76c7521756cf6737443
Medhavi1906 <medhavi.dubey19@gmail.com>
2026-04-12 12:26:37 +0530

    Add navigation bar to navbar.php

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="dashboard.php">EMS</a>
+
+    <div>
+      <a href="dashboard.php" class="btn btn-outline-light me-2">Home</a>
+      <a href="events.php" class="btn btn-outline-light me-2">Events</a>
+      <a href="my_tickets.php" class="btn btn-outline-light me-2">My Tickets</a>
+      <a href="logout.php" class="btn btn-danger">Logout</a>
+    </div>
+  </div>
+</nav>
